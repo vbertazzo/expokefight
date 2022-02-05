fb51640be41711287530ef395e4922d29266eaab
vbertazzo <vbertazzo@gmail.com>
2022-02-05 14:02:13 -0300

    Add get show battle module

--- lib/expokefight/battles/get.ex
+++ lib/expokefight/battles/get.ex
@@ -1,5 +1,5 @@
 defmodule Expokefight.Battles.Get do
-  alias Expokefight.{Battle, Repo}
+  alias Expokefight.{Battle, Error, Repo}
 
   def all() do
     battles =
@@ -8,5 +8,17 @@ defmodule Expokefight.Battles.Get do
       |> Repo.preload([:victorious, :defeated])
 
     {:ok, battles}
+  end
+
+  def by_id(id) do
+    result =
+      Battle
+      |> Repo.get(id)
+      |> Repo.preload([:victorious, :defeated])
+
+    case result do
+      nil -> {:error, Error.build(:not_found, "Battle not found")}
+      battle -> {:ok, battle}
+    end
   end
 end
--- test/expokefight/battles/get_test.exs
+++ test/expokefight/battles/get_test.exs
@@ -4,7 +4,7 @@ defmodule Expokefight.Battles.GetTest do
   alias Expokefight.{Battle, Pokemon}
   alias Expokefight.Battles.Get
 
-  describe("all/0") do
+  describe "all/0" do
     setup do
       insert(:battle)
       insert(:battle)
@@ -60,6 +60,54 @@ defmodule Expokefight.Battles.GetTest do
                   victorious_id: _pokemon_id8
                 }
               ]} = response
+    end
+  end
+
+  describe "by_id/1" do
+    setup do
+      insert(:battle)
+
+      :ok
+    end
+
+    test "returns the battle when a valid id is given" do
+      {:ok, [battle | _tail]} = Get.all()
+      id = battle.id
+
+      response = Get.by_id(id)
+
+      assert {:ok,
+              %Battle{
+                id: ^id,
+                defeated: %Pokemon{
+                  id: _pokemon_id1,
+                  image:
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                  name: "pikachu",
+                  type: "electric"
+                },
+                defeated_id: _pokemon_id2,
+                inserted_at: ~N[2022-02-04 20:08:02],
+                updated_at: ~N[2022-02-04 20:08:02],
+                victorious: %Pokemon{
+                  id: _pokemon_id3,
+                  image:
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+                  name: "charmander",
+                  type: "fire"
+                },
+                victorious_id: _pokemon_id4
+              }} = response
+    end
+
+    test "returns an error when an invalid id is given" do
+      id = "ebd41cf6-1fb4-4c98-90be-76882b43f0a9"
+
+      response = Get.by_id(id)
+
+      expected = {:error, %Expokefight.Error{result: "Battle not found", status: :not_found}}
+
+      assert response == expected
     end
   end
 end
