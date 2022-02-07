8bcdf925b7c31bfd728c390a8c64c07baf259a55
vbertazzo <vbertazzo@gmail.com>
2022-02-07 16:27:20 -0300

    Refactor transform data module

--- lib/expokefight/battles/transform_data.ex
+++ lib/expokefight/battles/transform_data.ex
@@ -1,19 +1,45 @@
 defmodule Expokefight.Battles.TransformData do
+  @moduledoc """
+  Functions for transforming the data coming from the PokeAPI
+  """
+
   alias Expokefight.Error
 
+  @doc """
+  Removes unnecessary fields from PokeAPI
+  """
   def call(pokemons) do
     pokemons
     |> Enum.map(&transform_pokemon_data/1)
   end
 
+  @doc """
+  Ensures correct arguments and returns the pokemon names in downcase
+
+  ## Example
+
+    iex> Expokefight.Battles.TransformData.normalize_params(%{"pokemon1" => "CHARMANDER", "pokemon2" => "ditto"})
+    {:ok, ["charmander", "ditto"]}
+
+    iex> Expokefight.Battles.TransformData.normalize_params(%{"pokemon1" => "CHARMANDER"})
+    {:error, Error.build(:bad_request, "Invalid parameters")
+
+  """
   def normalize_params(%{"pokemon1" => pokemon1, "pokemon2" => pokemon2}) do
-    pokemon1 = String.downcase(pokemon1)
-    pokemon2 = String.downcase(pokemon2)
+    pokemon1 = downcase_and_trim(pokemon1)
+
+    pokemon2 = downcase_and_trim(pokemon2)
 
     {:ok, [pokemon1, pokemon2]}
   end
 
   def normalize_params(_params), do: {:error, Error.build(:bad_request, "Invalid parameters")}
+
+  defp downcase_and_trim(string) do
+    string
+    |> String.downcase()
+    |> String.trim()
+  end
 
   defp transform_pokemon_data(pokemon) do
     pokemon
