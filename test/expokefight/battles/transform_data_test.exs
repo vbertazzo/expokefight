4f6b839c05f5f929dff51d5f8cc7ac07f44a24b5
vbertazzo <vbertazzo@gmail.com>
2022-02-07 16:27:30 -0300

    Add new test case for transform data module

--- test/expokefight/battles/transform_data_test.exs
+++ test/expokefight/battles/transform_data_test.exs
@@ -1,8 +1,8 @@
 defmodule Expokefight.Battles.TransformDataTest do
   use ExUnit.Case, async: true
   import Expokefight.Factory
-
   alias Expokefight.Battles.TransformData
+  alias Expokefight.Error
 
   describe "call/1" do
     test "returns a list of two pokemons" do
@@ -25,6 +25,38 @@ defmodule Expokefight.Battles.TransformDataTest do
           type: "grass/poison"
         }
       ]
+
+      assert response == expected
+    end
+  end
+
+  describe "normalize_params/1" do
+    test "returns a list of downcased pokemon names" do
+      params = %{"pokemon1" => "CHARMANDER", "pokemon2" => " dItTo   "}
+
+      response = TransformData.normalize_params(params)
+
+      expected = {:ok, ["charmander", "ditto"]}
+
+      assert response == expected
+    end
+
+    test "returns an error when there are parameters missing" do
+      params = %{"pokemon1" => "CHARMANDER"}
+
+      response = TransformData.normalize_params(params)
+
+      expected = {:error, %Error{result: "Invalid parameters", status: :bad_request}}
+
+      assert response == expected
+    end
+
+    test "returns an error when there is an invalid parameter" do
+      params = %{"pokemon1" => "CHARMANDER", "digimon2" => "agumon"}
+
+      response = TransformData.normalize_params(params)
+
+      expected = {:error, %Error{result: "Invalid parameters", status: :bad_request}}
 
       assert response == expected
     end
